30be84b81b210dbdb68ea7c2cbf811d47160a3a2
Furkan ARTAR <asosyalgenctvtr@gmail.com>
2021-12-07 09:18:14 +0300

    Validation structure has been added.

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
@@ -35,7 +29,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const Messages = require("../core/utilities/constants/messages.js");
+const express = require('express');
+const router = express.Router();
+
+const messages = new Messages;
 
 router.get('/', function(req, res, next) {
   res.send('getall');
@@ -8,16 +11,16 @@ router.get('/getbyid/:id', function(req, res, next) {
   res.send(req.params.id); 
 });
 router.post('/', function(req, res, next) {
-  res.send('add');
+  res.send(messages.add("xxx"));
 });
 router.put('/', function(req, res, next) {
-  res.send('update');
+  res.send(messages.add("xxx"));
 });
 router.delete('/', function(req, res, next) {
-  res.send('delete');
+  res.send(messages.delete("xxx"));
 });
 router.all('*', function(req, res, next) {
-  res.send('404');
+  res.send(messages.notFoundRoute);
 });
 
 module.exports = router;
